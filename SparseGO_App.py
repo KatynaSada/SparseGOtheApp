5d1fe0d541a66a945f4376be1222aecf313c8dde
Katyna Sada Del Real <ksada@tecnun.es>
2024-09-09 15:57:51 +0200

    file

--- SparseGO_App.py
+++ SparseGO_App.py
@@ -109,11 +109,15 @@ def load_all_data(inputdir, resultsdir, omics_type, device, typed=""):
     # Define the file path
     file_path = genotype
 
-    # Check if the file exists
-    if os.path.isfile(file_path):
-        st.write("The file exists.")
+# Check if the file exists
+    if file_path.is_file():
+        # Check if the file contains anything
+        if file_path.stat().st_size > 0:
+             st.write("The file exists and contains data.")
+        else:
+             st.write("The file exists but is empty.")
     else:
-        st.write("The file does not exist.")
+         st.write("The file does not exist.")
     st.write(genotype)
     st.write(cell_features)
     drug_features = np.genfromtxt(drug2fingerprint, delimiter=',')  # Load drug features
