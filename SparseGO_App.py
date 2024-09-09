b92f04e07c12aa9e090d9c2f15bb3b9bb07caf2d
Katyna Sada Del Real <ksada@tecnun.es>
2024-09-09 15:51:55 +0200

    d

--- SparseGO_App.py
+++ SparseGO_App.py
@@ -197,7 +197,6 @@ elif menu =='Drug Response':
         model = load_model(resultsdir, device)
         st.write(cell_features)
         AUDRC_cell = get_audrc_for_cell(cell_name, cell2id_mapping, cell_features, drug_features, drug2id_mapping, drugs_data, model, device)
-        st.write(AUDRC_cell) 
         slider_num = st.slider("Number of drugs", value=15,max_value=len(drug2id_mapping))
         # Get the first 10 drugs and their AUDRC values
         top_drugs = AUDRC_cell.head(slider_num)
