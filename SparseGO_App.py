9d2e18bda34201dca368a9c9f93f67921d86f729
Katyna Sada Del Real <ksada@tecnun.es>
2024-09-11 17:27:57 +0200

    cancer type

--- SparseGO_App.py
+++ SparseGO_App.py
@@ -13,8 +13,6 @@ import pandas as pd
 import subprocess
 from io import StringIO
 
-
-
 device = torch.device('cuda:0' if torch.cuda.is_available() else "cpu")
 
 @st.cache_data
@@ -122,6 +120,8 @@ def load_all_data(inputdir, resultsdir, omics_type, device, typed=""):
     cell_features = np.genfromtxt(genotype, delimiter=',')  # Load cell features
     drug_features = np.genfromtxt(drug2fingerprint, delimiter=',')  # Load drug features
     drug2id_mapping = load_mapping(drug2id)  # Load drug ID mapping
+    # The above code is loading a mapping of cell IDs using a function called `load_mapping` and
+    # storing the result in a variable called `cell2id_mapping`.
     cell2id_mapping = load_mapping(cell2id)  # Load cell ID mapping
     drugs_data = get_compound_names(compounds_txt)  # Load drug names
     drugs_data.pop(0)  # Remove the first entry if it's a header or unwanted
@@ -230,9 +230,37 @@ def validate_uploaded_file(uploaded_file, example_file):
     except Exception as e:
         st.error(f"An error occurred: {str(e)}")
         return False
+def update_audrc():
+    st.session_state.AUDRC_cell = None  # Reset AUDRC_cell, created because the bar chart stayed there when choosing another data source
+@st.cache_data
+def get_cell_types(cell2id_mapping):
+    """
+    Create a dictionary of cell types from a mapping of cell names to IDs.
 
+    Parameters:
+    cell2id_mapping (dict): A dictionary where keys are cell names and values are IDs.
+
+    Returns:
+    tuple: A dictionary of cell types and a set of unique cancer types.
+    """
+    # Create a new dictionary to hold cell types
+    cell_types = {}
+    
+    # Populate the new dictionary with cancer types
+    for cell_name in cell2id_mapping.keys():
+        # Split the cell name by underscore and join everything after the first part
+        cancer_type = '_'.join(cell_name.split('_')[1:])  # Join all parts after the first underscore
+        
+        # Add to the new dictionary
+        cell_types[cell_name] = cancer_type
+    
+    # Get unique cancer types
+    unique_cancers = set(cell_types.values())
+    
+    return cell_types, unique_cancers
+    
 # Download required data from GitLab
-REPO_URL = 'https://gitlab.com/katynasada/sparsego4streamlit.git'  # Replace with your repository URL
+REPO_URL = 'https://gitlab.com/katynasada/sparsego4streamlit.git'  # Replace with your repository URL=?
 BRANCH_NAME = 'main'  # Replace with the branch name
 clone_and_extract_folder(REPO_URL, BRANCH_NAME)
 
@@ -256,7 +284,7 @@ if menu =='About Us':
     
 elif menu =='Drug Response':
     st.title("Cancer Drug Response Prediction")
-    st.write("Use this tool to predict the response of a cell to more than 1500 drugs.")
+    st.subheader("Use this tool to predict the response of a cell to more than 1500 drugs.")
     st.write("Our neural networks predict a continuous value that represents the area under the dose-response curve (AUDRC) normalized such that **AUDRC = 0 represents complete cell death, AUDRC = 1 represents no effect, and AUDRC > 1 represents that the treatment favours cell growth**.")
     st.write("**Note:** If the predictions are computed for more than one sample, the mean for each drug is calculated.")
     model = st.selectbox('What type of omics data do you want to use?',('Mutations', 'Expression', 'Mutations and expression'))
@@ -270,23 +298,19 @@ elif menu =='Drug Response':
         # Load required model
         model = load_model(resultsdir, device)
         
-        input_type = st.selectbox('Select your data source for prediction:', ('Upload cells/patients data', 'Use CCLE cell lines'),index=None)
+        input_type = st.selectbox('Select your data source for prediction:', ('Upload cells/patients data', 'Use CCLE cell lines'),index=None,on_change=update_audrc)
 
         if input_type == "Upload cells/patients data":
             gene2id_file = f"{inputdir}gene2ind.txt"
             example_file = f"{inputdir}mycellexpression.txt"
             # Write instructions for the user
-            st.write("**Please upload the expression data for the 14,834 genes in a text file. Each line should represent a sample with expression values for all genes separated by commas.**")
+            st.write(f"**Please upload the expression data for the {len(pd.read_csv(gene2id_file, sep='\t'))+1} genes in a text file. Each line should represent a sample with expression values for all genes separated by commas.**")
             # Create two columns for the buttons
             col1, col2 = st.columns(2)
             with col1:
                 st.download_button("Download Required Gene List (Must Be in This Order)", open(gene2id_file), file_name="gene2id.txt")
             with col2:
                 st.download_button("Download Example Features File", open(example_file), file_name="mycellexpression.txt")
-            
-            # Initialize session state for storing results
-            if 'AUDRC_cell' not in st.session_state:
-                st.session_state.AUDRC_cell = None
 
             # File uploader for user to upload their cell features
             uploaded_file = st.file_uploader("Upload Your Cell Features Here")
@@ -301,17 +325,24 @@ elif menu =='Drug Response':
                 st.write(st.session_state.AUDRC_cell)
                 slider_num = st.slider("Number of drugs", value=10, max_value=len(drug2id_mapping), key="drug_slider")
                 generate_audrc_bar_chart(st.session_state.AUDRC_cell, slider_num)
-            
-                
 
         elif input_type == "Use CCLE cell lines":
-            # Initialize session state for storing results
-            if 'AUDRC_cell' not in st.session_state:
-                st.session_state.AUDRC_cell = None
-            cell_names = st.multiselect('Select one or more cell lines',cell2id_mapping)
+            
+            col1, col2 = st.columns(2)
+            with col1:
+                cell_names = st.multiselect('Select one or more cell lines 🦠',cell2id_mapping, key="multiselect_cells")
+            with col2:
+                cell_types, unique_cancers = get_cell_types(cell2id_mapping)
+                selected_cancer_type = st.multiselect('**and/or** select all cell lines of a cancer type 🧠 🫁 🩸 🦴',unique_cancers, key="multiselect_cancer")
+                cell_names_cancer = [cell_name for cell_name, cancer in cell_types.items() if cancer in selected_cancer_type]
+            
+            # Combine lists without duplicates using set
+            all_cell_names = list(set(cell_names) | set(cell_names_cancer))
+            st.info(f"You're predicting the drug response of {len(all_cell_names)} cell lines.")
+            
             if st.button('Predict drug response 💊'):
                 cell_specific_features = []
-                for name in cell_names:
+                for name in all_cell_names:
                     cell_idx = cell2id_mapping.get(name)  # Get the index of the cell from the cell name using a mapping dictionary
                     cell_specific_features.append(cell_features[cell_idx])  # Retrieve the specific features for the cell at the given index
                 st.session_state.AUDRC_cell = get_audrc_mean(cell_specific_features, drug_features, drug2id_mapping, drugs_data, model, device)
